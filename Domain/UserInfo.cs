404b6b69c26dc2ea3112ce8694263e02728cf936
lenapapuc <elenapapuc99@gmail.com>
2024-08-11 13:42:41 +0300

    First commit

---
+++ Domain/UserInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection.Metadata;
+
+namespace Domain
+{
+    public class UserInfo
+    {
+        public string LastName { get; set; } 
+        public string FirstName { get; set; }
+        public string? MiddleName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public Address? Address { get; set; }
+        public DateTime? BirthDate { get; set; }
+    }
+}
